429159b658683fd015830fe5ad7db6584242e0ef
Fablinov Mikhail <fm@kpd.digital>
2024-11-17 01:31:28 +0300

    fixes lint

--- src/processing.py
+++ src/processing.py
@@ -1,20 +1,16 @@
-from typing import Iterable, Any
+from typing import Any, Iterable
 
 
-def filter_by_state(dictionaries: Iterable[dict[Any, Any]], state: Any='EXECUTED') -> dict[Any, Any]:
+def filter_by_state(dictionaries: Iterable[dict[Any, Any]], state: Any = "EXECUTED") -> list[dict[Any, Any]]:
     """Функция выведения данных по определенному значению"""
     executed_list = []
     for i in dictionaries:
-        if i ['state'] == state:
+        if i["state"] == state:
             executed_list.append(i)
     return executed_list
 
 
-def sort_by_date(dictionaries: Iterable[dict[Any, Any]], reverse: bool=True) -> dict[Any, Any]:
+def sort_by_date(dictionaries: Iterable[dict[Any, Any]], reverse: bool = True) -> list[dict[Any, Any]]:
     """Функция для сортировки по датам"""
-    list_date =sorted(dictionaries, key=lambda x: x['date'], reverse=True)
+    list_date = sorted(dictionaries, key=lambda x: x["date"], reverse=True)
     return list_date
-
-
-
-
