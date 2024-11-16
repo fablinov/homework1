3f37a84a9b7da92e9f764e5d1455cd247c1406be
Fablinov Mikhail <fm@kpd.digital>
2024-11-17 01:17:45 +0300

    fixes function

---
+++ src/processing.py
@@ -0,0 +1,20 @@
+from typing import Iterable, Any
+
+
+def filter_by_state(dictionaries: Iterable[dict[Any, Any]], state: Any='EXECUTED') -> dict[Any, Any]:
+    """Функция выведения данных по определенному значению"""
+    executed_list = []
+    for i in dictionaries:
+        if i ['state'] == state:
+            executed_list.append(i)
+    return executed_list
+
+
+def sort_by_date(dictionaries: Iterable[dict[Any, Any]], reverse: bool=True) -> dict[Any, Any]:
+    """Функция для сортировки по датам"""
+    list_date =sorted(dictionaries, key=lambda x: x['date'], reverse=True)
+    return list_date
+
+
+
+
